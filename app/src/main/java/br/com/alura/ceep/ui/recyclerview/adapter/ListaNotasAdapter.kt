f046b9b4287977b00ca93723ef64bc25b9ec2125
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-07-02 12:47:01 -0300

    - Aula 2

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -2,22 +2,12 @@ package br.com.alura.ceep.ui.recyclerview.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import br.com.alura.ceep.BR
-import br.com.alura.ceep.R
+import br.com.alura.ceep.databinding.ItemNotaBinding
 import br.com.alura.ceep.model.Nota
-import br.com.alura.ceep.ui.extensions.carregaImagem
-import kotlinx.android.synthetic.main.item_nota.view.*
 
 class ListaNotasAdapter(
     private val context: Context,
@@ -26,7 +16,7 @@ class ListaNotasAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
-        val viewDataBinding = DataBindingUtil.inflate<ViewDataBinding>(inflater, R.layout.item_nota, parent, false)
+        val viewDataBinding = ItemNotaBinding.inflate(inflater, parent, false)
         return ViewHolder(viewDataBinding)
     }
 
@@ -36,18 +26,10 @@ class ListaNotasAdapter(
         }
     }
 
-    inner class ViewHolder(private val viewDataBinding: ViewDataBinding) : RecyclerView.ViewHolder(viewDataBinding.root) {
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
 
         private lateinit var nota: Nota
-        private val campoDescricao: TextView by lazy {
-            itemView.item_nota_descricao
-        }
-        private val campoFavorita: ImageView by lazy {
-            itemView.item_nota_favorita
-        }
-        private val campoImagem: ImageView by lazy {
-            itemView.item_nota_imagem
-        }
 
         init {
             itemView.setOnClickListener {
@@ -59,19 +41,7 @@ class ListaNotasAdapter(
 
         fun vincula(nota: Nota) {
             this.nota = nota
-            viewDataBinding.setVariable(BR.nota, nota)
-            campoDescricao.text = nota.descricao
-            if (this.nota.favorita) {
-                campoFavorita.visibility = VISIBLE
-            } else {
-                campoFavorita.visibility = GONE
-            }
-            campoImagem.carregaImagem(nota.imagemUrl)
-            if (nota.imagemUrl.isEmpty()) {
-                campoImagem.visibility = GONE
-            } else {
-                campoImagem.visibility = VISIBLE
-            }
+            viewDataBinding.nota = nota
         }
 
     }
